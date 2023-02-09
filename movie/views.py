084ba314a1e7b35852e77b8430a1c415ace6d5b8
Brahiam Quintero <brahiamquintero@hotmail.com>
2023-02-09 11:35:16 -0500

    cambio funcion home 2

--- movie/views.py
+++ movie/views.py
@@ -7,4 +7,4 @@ from django.http import HttpResponse
 def home(request):
     # return HttpResponse('<h1>Welcome to Home Page</h1>')
     # return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Paola Vallejo'})
+    return render(request, 'home.html', {'name': 'Brahiam Quintero'})
